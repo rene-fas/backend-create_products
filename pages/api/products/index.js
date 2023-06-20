a11736aee50412d778c74b9034205562bee064d2
René Faßbender <rene-fassb@gmx.de>
2023-06-20 11:19:06 +0200

    feat: add dog feature

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,5 +7,16 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const products = await Product.find();
     return response.status(200).json(products);
+  } else if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      const newProduct = await Product.create(productData);
+      return response.status(201).json({ status: "Product created." });
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
+  } else {
+    return response.status(405).json({ error: "Method not allowed." });
   }
 }
