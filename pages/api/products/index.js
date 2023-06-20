3f58e13d6fdc6f3acd51c6d9c4ab68095ca2cfa0
René Faßbender <rene-fassb@gmx.de>
2023-06-20 11:21:56 +0200

    refactor: if/else to switchcase

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,19 +4,20 @@ import Product from "../../../db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const products = await Product.find();
-    return response.status(200).json(products);
-  } else if (request.method === "POST") {
-    try {
-      const productData = request.body;
-      const newProduct = await Product.create(productData);
-      return response.status(201).json({ status: "Product created." });
-    } catch (error) {
-      console.error(error);
-      return response.status(400).json({ error: error.message });
-    }
-  } else {
-    return response.status(405).json({ error: "Method not allowed." });
+  switch (request.method) {
+    case "GET":
+      const products = await Product.find();
+      return response.status(200).json(products);
+    case "POST":
+      try {
+        const productData = request.body;
+        const newProduct = await Product.create(productData);
+        return response.status(201).json({ status: "Product created." });
+      } catch (error) {
+        console.error(error);
+        return response.status(400).json({ error: error.message });
+      }
+    default:
+      return response.status(405).json({ error: "Method not allowed." });
   }
 }
